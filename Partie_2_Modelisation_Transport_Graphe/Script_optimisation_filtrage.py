e720f8e5578af655b30a5f5d85aa4267f530f928
Clement R <104372704+IDEFIX95@users.noreply.github.com>
2025-05-31 15:12:32 +0200

    Update Script_optimisation_filtrage.py

--- Partie_2_Modelisation_Transport_Graphe/Script_optimisation_filtrage.py
+++ Partie_2_Modelisation_Transport_Graphe/Script_optimisation_filtrage.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
+
+# PArtie 2 de la question 1 de la Partie 2
+
 # === 1. Charger le fichier CSV existant ===
 input_path = "H:/Documents/ING1/Projet_Mai_2025/connection_lignes_avec_metro_associe.csv"
 df = pd.read_csv(input_path, sep=';', encoding='utf-8')
 
 # === 2. Afficher les colonnes détectées ===
-print("\n📋 Colonnes détectées :", df.columns.tolist())
+print("\n Colonnes détectées :", df.columns.tolist())
 
 # === 3. Normaliser les noms de colonnes ===
 df.columns = df.columns.str.strip().str.lower()
@@ -13,7 +16,7 @@ df.columns = df.columns.str.strip().str.lower()
 # === 4. Vérification directe des colonnes ===
 required_cols = {'station1', 'station2'}
 if not required_cols.issubset(df.columns):
-    raise ValueError(f"❌ Les colonnes attendues {required_cols} sont manquantes.\nColonnes actuelles : {df.columns.tolist()}")
+    raise ValueError(f" Les colonnes attendues {required_cols} sont manquantes.\nColonnes actuelles : {df.columns.tolist()}")
 
 # === 5. Créer un booléen "connected" si absent
 if 'connected' not in df.columns:
@@ -31,5 +34,5 @@ df_unique.columns = ['station1', 'station2', 'connected']
 output_path = "H:/Documents/ING1/Projet_Mai_2025/graphe_connexions_optimise.csv"
 df_unique.to_csv(output_path, index=False, sep=';', encoding='utf-8-sig')
 
-print("\n✅ Fichier optimisé créé avec succès :")
+print("\n Fichier optimisé créé avec succès :")
 print(f"➡ {output_path}")
